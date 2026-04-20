52f4fa6a03cbd406025ff139d1008dec458cb137
AILE1209 <tsubasaikeda1209@gmail.com>
2026-04-20 13:08:39 +0900

    fader

--- Src/Common/Fader.cpp
+++ Src/Common/Fader.cpp
@@ -25,56 +25,49 @@ void Fader::Init(void)
 
 void Fader::Update(void)
 {
-
-	if (isEnd_)
+	if(isEnd_)
 	{
-		// フェード処理が終了していたら何もしない
+		//フェード処理が終了していたら何もしない
 		return;
 	}
 	
-	if (isPreEnd_)
+	if(isPreEnd_)
 	{
-		// 1フレーム後(Draw後)に終了とする
+		//1フレーム後(Draw後)に終了とする
 		isEnd_ = true;
 		return;
 	}
 
 	switch (state_)
 	{
-
 	case STATE::NONE:
 		return;
-
 	case STATE::FADE_OUT:
 		alpha_ += SPEED_ALPHA;
 		if (alpha_ > 255)
 		{
-			// フェード終了
+			//フェード終了
 			alpha_ = 255;
 			isPreEnd_ = true;
 		}
 		break;
-
 	case STATE::FADE_IN:
 		alpha_ -= SPEED_ALPHA;
 		if (alpha_ < 0)
 		{
-			// フェード終了
+			//フェード終了
 			alpha_ = 0;
 			isPreEnd_ = true;
 		}
 		break;
-
 	default:
 		return;
 
 	}
-
 }
 
 void Fader::Draw(void)
 {
-
 	switch (state_)
 	{
 	case STATE::NONE:
@@ -82,15 +75,13 @@ void Fader::Draw(void)
 	case STATE::FADE_OUT:
 	case STATE::FADE_IN:
 		SetDrawBlendMode(DX_BLENDMODE_ALPHA, static_cast<int>(alpha_));
-		DrawBox(
-			0, 0,
+		DrawBox(0,0,
 			Application::SCREEN_SIZE_X,
 			Application::SCREEN_SIZE_Y,
 			0x000000, true);
 		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 		break;
 	}
-
 }
 
 Fader::STATE Fader::GetState(void)
